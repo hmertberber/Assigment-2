616c03491210f6e5e75edc764646757d0759f240
hmertberber <huberber@stu.okan.edu.tr>
2023-03-30 14:09:32 +0300

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,27 @@
+final List<Map<String, dynamic>> questions = [
+  {
+    'question': 'What is the capital of France?',
+    'options': ['Paris', 'Berlin', 'Madrid', 'Rome'],
+    'answer': 'Paris',
+  },
+  {
+    'question': 'What is the highest mountain in the world?',
+    'options': ['Mount Everest', 'K2', 'Kangchenjunga', 'Lhotse'],
+    'answer': 'Mount Everest',
+  },
+  {
+    'question': 'Which planet is known as the Red Planet?',
+    'options': ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+    'answer': 'Mars',
+  },
+  {
+    'question': 'What is the largest country in South America?',
+    'options': ['Brazil', 'Argentina', 'Peru', 'Colombia'],
+    'answer': 'Brazil',
+  },
+  {
+    'question': 'What is the smallest country in the world by land area?',
+    'options': ['Monaco', 'Nauru', 'Vatican City', 'San Marino'],
+    'answer': 'Vatican City',
+  },
+];
